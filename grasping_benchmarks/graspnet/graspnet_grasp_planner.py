8a6190166780e70123066b21aae79847285b031f
fbottarel <fabrizio.bottarel@iit.it>
2021-08-16 11:30:56 +0200

    removed ipdb statements

--- grasping_benchmarks/graspnet/graspnet_grasp_planner.py
+++ grasping_benchmarks/graspnet/graspnet_grasp_planner.py
@@ -2,7 +2,7 @@
 # This software may be modified and distributed under the terms of the
 # LGPL-2.1+ license. See the accompanying LICENSE file for details.
 
-import ipdb
+from traitlets.traitlets import Bool
 import yaml
 import os
 import sys
