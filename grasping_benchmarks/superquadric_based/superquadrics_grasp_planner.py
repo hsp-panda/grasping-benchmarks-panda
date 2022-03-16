fixed point cloud acquisition and parsing for superquadric grasp computation

grasping_benchmarks/superquadric_based/superquadrics_grasp_planner.py
@@ -90,8 +90,8 @@ class SuperquadricsGraspPlanner(BaseGraspPlanner):
 
         Parameters
         ---------
-            pointcloud (np.ndarray): array of 3D points
-            colors (np.ndarray):array of RGB colors
+            pointcloud (np.ndarray): array of 3D points of shape (n,3)
+            colors (np.ndarray):array of RGB colors of shape (n,3)
             cam_pos (np.ndarray): 3D position of the camera expressed wrt the robot base
             cam_quat (np.ndarray): quaternion (x, y, z, w) of the camera orientation expressed wrt the robot base
 
@@ -122,26 +122,27 @@ class SuperquadricsGraspPlanner(BaseGraspPlanner):
         sb_points = sb.deque_Vector3d()
         sb_colors = sb.vector_vector_uchar()
 
-        for i in range(0, pointcloud.shape[1]):
-            for j in range(0, pointcloud.shape[2]):
-                pt = np.array([pointcloud[0][i][j], pointcloud[1][i][j], pointcloud[2][i][j]])
+        for i in range(0, pointcloud.shape[0]):
 
-                icub_pt = icub_T_cam.dot(np.append(pt, 1))
-                if np.isnan(icub_pt[0]):
-                    continue
+            pt = np.array([pointcloud[i,0], pointcloud[i,1], pointcloud[i,2]])
 
-                if colors.size != 0:
-                    col = colors[i][:3]
-                    if type(col) is np.ndarray:
-                        col = col.astype(int)
-                        col = col.tolist()
-                    else:
-                        col = [int(c) for c in col]
+            icub_pt = icub_T_cam.dot(np.append(pt, 1))
+            if np.isnan(icub_pt[0]):
+                continue
+
+            if colors.size != 0:
+                col = colors[i][:3]
+                if type(col) is np.ndarray:
+                    col = col.astype(int)
+                    col = col.tolist()
                 else:
-                    col = [255,255,0]
+                    col = [int(c) for c in col]
+            else:
+                col = [255,255,0]
 
-                sb_points.push_back(icub_pt[:3])
-                sb_colors.push_back(col)
+            sb_points.push_back(icub_pt[:3])
+            sb_colors.push_back(col)
+
 
         if sb_points.size() >= self.cfg['sq_model']['minimum_points']:
             self._pointcloud.setPoints(sb_points)

grasping_benchmarks_ros/superquadrics_based_ros/superq_grasp_planner_service.py
@@ -17,6 +17,7 @@ import ros_numpy
 
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Header
+from sensor_msgs.msg import PointCloud2
 
 from grasping_benchmarks_ros.srv import GraspPlanner
 from grasping_benchmarks_ros.msg import BenchmarkGrasp
@@ -64,14 +65,17 @@ class SuperquadricGraspPlannerService(SuperquadricsGraspPlanner):
         """
         # Get the ROS data
         ros_cloud = req.cloud
-        cam_position = np.array([req.view_point.position.x, req.view_point.position.y, req.view_point.position.z])
-        cam_quat = np.array([req.view_point.orientation.x, req.view_point.orientation.y,
-                             req.view_point.orientation.z, req.view_point.orientation.w])
+        cam_position = np.array([req.view_point.pose.position.x,
+                                 req.view_point.pose.position.y,
+                                 req.view_point.pose.position.z])
+        cam_quat = np.array([req.view_point.pose.orientation.x,
+                             req.view_point.pose.orientation.y,
+                             req.view_point.pose.orientation.z,
+                             req.view_point.pose.orientation.w])
 
-        # pointcloud2 to numpy array
-        pc_data = ros_numpy.numpify(ros_cloud) # TODO: fix color decoding as with this method colors result in NaNs
+        # pointcloud2 to numpy array of shape (n_points, 3)
+        points = self.npy_from_pc2(ros_cloud)
 
-        points = np.array([pc_data['x'], pc_data['y'], pc_data['z']])
         camera_data = self.create_camera_data(points, cam_position, cam_quat)
 
         return camera_data
@@ -145,6 +149,37 @@ class SuperquadricGraspPlannerService(SuperquadricsGraspPlanner):
             self.grasp_pose_publisher.publish(p)
 
         return grasp_msg
+
+    def npy_from_pc2(self, pc : PointCloud2) -> Tuple[np.ndarray, np.ndarray]:
+        """Conversion from PointCloud2 to a numpy format
+
+        Parameters
+        ----------
+        pc : PointCloud2
+            Scene or object pc
+
+        Returns
+        -------
+        Tuple[np.array, np.array]
+            Point cloud in a nx3 array, where rows are xyz, and nx3 array where
+            rows are rgb
+        """
+
+        pc_data = ros_numpy.point_cloud2.pointcloud2_to_array(pc)
+
+        # Decode x,y,z
+        # NaNs are removed later
+        points_xyz = ros_numpy.point_cloud2.get_xyz_points(pc_data, remove_nans=False)
+
+        # Decode r,g,b
+        pc_data_rgb_split = ros_numpy.point_cloud2.split_rgb_field(pc_data)
+        points_rgb = np.column_stack((pc_data_rgb_split['r'], pc_data_rgb_split['g'], pc_data_rgb_split['b']))
+
+        # Find NaNs and get remove their indexes
+        valid_point_indexes = np.argwhere(np.invert(np.bitwise_or.reduce(np.isnan(points_xyz), axis=1)))
+        valid_point_indexes = np.reshape(valid_point_indexes, valid_point_indexes.shape[0])
+
+        return points_xyz[valid_point_indexes], points_rgb[valid_point_indexes]
 
 
 if __name__ == "__main__":