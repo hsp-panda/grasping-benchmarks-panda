c5439a83543314abe3c1fa33c68498293461fe40
fbottarel <fabrizio.bottarel@iit.it>
2022-05-27 16:36:49 +0200

    now supports compilation of tf operators at startup time

--- docker/6dgraspnet/entrypoint.sh
+++ docker/6dgraspnet/entrypoint.sh
@@ -9,6 +9,15 @@ fi
 # Initialize the container
 echo "==> Configuring 6DoFGraspNet benchmarking image"
 setup.sh
+
+# We need to compile additional tensorflow operators in this one
+echo "==> Compiling custom TensorFlow operators"
+CURR_DIR=$PWD
+cd /workspace/sources/6dof-graspnet
+sh compile_pointnet_tfops.sh
+cd $CURR_DIR
+echo "==> Custom TensorFlow operators ready"
+
 echo "==> 6DoFGraspNet benchmarking container ready"
 
 # If a CMD is passed, execute it
